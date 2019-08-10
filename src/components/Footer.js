d59ef3c9753bb97ee9cd2235bc2bfc8126b08e29
yzhan407 <hehehe2333@hotmail.com>
2019-08-09 23:53:47 -0700

    add footer animation to be fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,10 @@ class Footer extends React.Component{
   constructor(props){
     super(props);
     this.handleScrollEvent=this.handleScrollEvent.bind(this);
+    this.handleResize=this.handleResize.bind(this);
+    this.state={
+      rotation: 0,
+    }
   }
 
   state={
@@ -17,12 +21,43 @@ class Footer extends React.Component{
     window.removeEventListener("scroll",this.handleScrollEvent);
   };
   handleScrollEvent(){
-    console.log(window.pageYOffset);
+    switch(Math.round(window.pageYOffset/500)){
+      case 0:
+        this.setState({
+          rotation: 0,
+        });
+        break;
+      case 1:
+        this.setState({
+          rotation: 72,
+        });
+        break;
+      case 2:
+        this.setState({
+          rotation: 144,
+        });
+        break;
+      case 3:
+        this.setState({
+          rotation: 216,
+        });
+        break;
+      case 4:
+        this.setState({
+          rotation: 288,
+        });
+        break;
+      default:
+        this.setState({
+          rotation: 0,
+        });
+      break;
+    }
   };
   render(){
     return(
       <div className="footer-main">
-        <img className="earth-img" ref="image0" src={earth} alt="earthImage" onScroll={this.handleScrollEvent }/>
+        <img className={'earth-img-'+this.state.rotation} ref="image0" src={earth} alt="earthImage" onScroll={this.handleScrollEvent }/>
       </div>
     )
   }
