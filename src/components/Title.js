09ca610bb07923d1824648213e77666d87b1ae64
yzhan407 <hehehe2333@hotmail.com>
2019-08-20 22:49:10 -0700

    fix link

--- src/components/Title.js
+++ src/components/Title.js
@@ -8,7 +8,7 @@ class Title extends React.Component{
         return(
             <div id="title">
                 <img src={Mylogo} alt="mylogo" className="title-logo"/>
-                <a className="porfolio-link" href="www.yifanzhang47.com/myplace/porfolio/build/index.html">Porfolio</a>
+                <a className="porfolio-link" href="http://www.yifanzhang47.com/myplace/porfolio/build/index.html" target="_blank" rel="noopener noreferrer">Porfolio</a>
                 <h1>Yifan Zhang</h1>
                 <h6>何も捨てることができない人には、何も変えることはできないだろう  -- Shingeki no Kyojin 進撃の巨人</h6>
                 <a className="blog-git-link" href="https://github.com/yzhan407/newBlog">Github Page of This Blog </a>
